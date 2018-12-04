e61585e8fd2f980c88cde7a8fa6813b4163eb658
Georgiana Boboc <georgianageorgianaboboc@gmail.com>
2018-12-04 20:36:25 +0000

    tabela users

--- server.js
+++ server.js
@@ -1,4 +1,40 @@
 const express = require('express')
 const app = express()
+
+//creare BD
+
+
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize('calendar', 'root', '', {
+    dialect: "mysql",
+    host: "localhost"
+})
+
+sequelize.authenticate().then(function(){
+    console.log('succes')
+}).catch(function(){
+    console.log('there was an error connecting to db')
+})
+
+const Users=sequelize.define('users', {
+ id_user:Sequelize.INTEGER,
+username:Sequelize.STRING,
+password:Sequelize.STRING
+
+ })
+
+
+app.get('/createdb', (request, response) => {
+    sequelize.sync({force:true}).then(() => {
+        response.status(200).send('tables created')
+    }).catch((err) => {
+        console.log(err)
+        response.status(200).send('could not create tables')
+    })
+})
+
+
+
 app.use('/', express.static('static'))
 app.listen(8080)
