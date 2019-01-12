16fd8cd9f076e5263c3852e1c8bb93d64bbac153
Georgiana Boboc <georgianageorgianaboboc@gmail.com>
2019-01-12 08:43:11 +0000

    creare frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       I will be the app
+      </div>
+    );
+  }
+}
+
+export default App;
