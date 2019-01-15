b5fd4a94706294891f9eb495ae18347a265a8a2e
Adrian Antonescu <antonescu.adrian16@gmail.com>
2019-01-15 21:18:31 +0000

    mmsda

--- frontend/src/containers/index.js
+++ frontend/src/containers/index.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
 //import moment from "moment";
 //import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
-import Icon from '@material-ui/core/Icon';
-import DeleteIcon from '@material-ui/icons/Delete';
-import NavigationIcon from '@material-ui/icons/Navigation';
+
 
 class Index extends Component {
  //    constructor(props) {
