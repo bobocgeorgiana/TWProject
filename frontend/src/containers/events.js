cfa017106cbf3e2ad2a1d3a3affab85905e1e59d
Adrian Antonescu <antonescu.adrian16@gmail.com>
2019-01-16 09:16:10 +0000

    events

--- frontend/src/containers/events.js
+++ frontend/src/containers/events.js
@@ -20,17 +20,17 @@ const styles = theme => ({
 });
 
 let id = 0;
-function createData(name, calories, fat, carbs, protein) {
+function createData(name, startTime, endTime, description) {
   id += 1;
-  return { id, name, calories, fat, carbs, protein };
+  return { id, name, startTime, endTime, description };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Fotbal', '17/01/2018 12:00', '17/01/2018 13:00', 'Campionat de fotbal juniori'),
+  createData('Concurs skandenberg', '17/01/2018 17:00', '17/01/2018 19:00', 'Invitat special Ion Oncescu'),
+  createData('Baschet', '21/01/2018 11:00', '21/01/2018 14:00', 'Activitate organizata de liceul LTCCB'),
+  createData('Handbal', '22/01/2018 10:00', '29/01/2018 13:00', 'Campionat regional'),
+  createData('Fotbal', '31/01/2018 12:00', '31/01/2018 13:00', ''),
 ];
 
 function SimpleTable(props) {
@@ -41,11 +41,10 @@ function SimpleTable(props) {
       <Table className={classes.table}>
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat (g)</TableCell>
-            <TableCell align="right">Carbs (g)</TableCell>
-            <TableCell align="right">Protein (g)</TableCell>
+            <TableCell>Event</TableCell>
+            <TableCell align="right">Starting date</TableCell>
+            <TableCell align="right">Ending date</TableCell>
+            <TableCell align="right">Description</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -54,10 +53,9 @@ function SimpleTable(props) {
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.startTime}</TableCell>
+              <TableCell align="right">{row.endTime}</TableCell>
+              <TableCell align="right">{row.description}</TableCell>
             </TableRow>
           ))}
         </TableBody>
